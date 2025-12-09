04a8fe015aab86a0e4f2fc7500c5aba129af3e8b
PuchkuBE <PuchkuBE@noreply.be>
2025-12-09 11:32:39 +0100

    Day 7

--- day7.py
+++ day7.py
@@ -7,15 +7,22 @@ def nr_splits(input):
     for actions in input[1:]:
         newstate = oldstate[:]
         for idx, action in enumerate(actions):
-            if action == '^' and oldstate[idx] == '|':
-                newstate = newstate[:idx-1] + '|.|'+newstate[idx+2:] 
+            if action == '^' and type(oldstate[idx]) is int and oldstate[idx] > 0: 
+                newstate[idx-1] += oldstate[idx]
+                newstate[idx] = 0
+                newstate[idx+1] += oldstate[idx]
                 ans1 += 1
         oldstate = newstate
-    return ans1
+    ans2 = sum(oldstate)
+    return ans1, ans2
 
 with open('input/day7.txt', 'r') as f:
     lines = f.read().splitlines()
 
-lines[0] = lines[0].replace('S','|')
-
-print(f'Solution for day7/part1: {nr_splits(lines)}')
+lines = [
+    [1 if c == 'S' else 0 if c == '.' else c for c in line]
+    for line in lines
+]
+ans1, ans2 = nr_splits(lines)
+print(f'Solution for day7/part1: {ans1}')
+print(f'Solution for day7/part2: {ans2}')
