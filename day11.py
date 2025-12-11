272a023ab0e9724d86165c9671fd34effb177dc7
PuchkuBE <PuchkuBE@noreply.be>
2025-12-11 16:37:43 +0100

    Day 11

--- day11.py
+++ day11.py
@@ -10,14 +10,20 @@ for line in open('input/day11.txt','r').read().splitlines():
     for to in tos:
         G.add_edge(frm, to)
 
-def searchpath(node , to):
+def searchpath(node , to, fft, dac, cache: set):
     succ = nx.DiGraph.successors(G, node)  
     nr_paths = 0
+    if (node, fft, dac) in cache:
+        return cache[(node, fft, dac)]
+    fft = fft or node == 'fft'
+    dac = dac or node == 'dac'
     for s in succ:
-        if s == to:
+        if s == to and fft and dac:
             nr_paths += 1
         else:
-            nr_paths += searchpath(s, to)
+            nr_paths += searchpath(s, to, fft, dac, cache)
+    cache[(node, fft, dac)] = nr_paths            
     return nr_paths
 
-print(searchpath('you','out'))
+print(f'Solution for day10/part1: {searchpath('you','out',True, True, {})}')
+print(f'Solution for day11/part2: {searchpath('svr','out', False, False, {})}')
